6a05030ab822a8a98eabfbb5973e0e630d263403
William <williamoconnor@me.com>
2024-10-01 00:04:41 +0100

    Update install_openvpn_access_server.sh

--- openvpn-access-server-ubuntu/install_openvpn_access_server.sh
+++ openvpn-access-server-ubuntu/install_openvpn_access_server.sh
@@ -3,7 +3,7 @@ userPassword=$1
 
 #download the packages
 cd /tmp
-wget -c http://swupdate.openvpn.org/as/openvpn-as-2.1.9-Ubuntu16.amd_64.deb
+wget -c http://ftp.uk.debian.org/debian/pool/main/o/openvpn/openvpn_2.6.12-1_amd64.deb
 
 #install the software
 sudo dpkg -i openvpn-as-2.1.9-Ubuntu16.amd_64.deb
